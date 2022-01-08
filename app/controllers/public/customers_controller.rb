27dc43a4e2d001558d5a7c4ef4235de5ed6ea55b
masaya0731 <masaya0806543@gmail.com>
2022-01-08 03:59:28 +0000

    [Add] ユーザー編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,9 +5,16 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    customer = Customer.find(params[:id])
+    if customer.update(customer_params)
+      redirect_to public_customer_path
+    else
+      render :edit
+    end
   end
 
   private
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f17ba981da662aeb96e57053a8cf1a609982def36d8ed8ccd853b968d2864d27c1e80a3137007f43b7b9832a11fa3527686e8c6bbb400fe2a9bd7014649f9335'
