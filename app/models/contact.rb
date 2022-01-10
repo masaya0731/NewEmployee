6317abc16a7d4aedf7f307c5ea31f96e1ba2f217
masaya0731 <masaya0806543@gmail.com>
2022-01-10 09:51:06 +0000

    [Fix] バリデーション

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,8 @@
 class Contact < ApplicationRecord
 
-  validates :name, presence: true
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :title, presence: true, length: { maximum: 200 }
+  validates :body, presence: true, length: { maximum: 500 }
   validates :email, presence: true
 
 end
