fb01d0828db212d0ce041537d858aebff4382bdf
masaya0731 <masaya0806543@gmail.com>
2022-01-16 12:25:54 +0000

    [Add] コメント機能

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,15 +1,19 @@
-class Public::CommentsController < ApplicationController
+class Public::PostCommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = current_customer.post_comments.new(post_comment_params)
-    @comment.post_id = post.id
-    @comment.save
-    redirect_to public_post_path(post)
+    @comment.post_id = @post.id
+    if @comment.save
+      redirect_to public_post_path(@post)
+      flash[:success] = "コメントしました"
+    end
   end
 
   def destroy
-    PostComment.find_by(id: params[:id]).destroy
-    redirect_to public_post_path(params[:post_id])
+    if PostComment.find_by(id: params[:id]).destroy
+      redirect_to public_post_path(params[:post_id])
+      flash[:danger] = '投稿を削除しました'
+    end
   end
 
   private
