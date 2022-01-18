54b746351ba51f71a180f3b935343a3a74d50183
masaya0731 <masaya0806543@gmail.com>
2022-01-18 11:23:21 +0000

    [Fix]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-if Rails.env.development?
+
   AdminUser.create!(email: ENV['ADMIN_ADRESS'], password: ENV['ADMIN_PASSWORD'],
                     password_confirmation: 'password')
-end
+
