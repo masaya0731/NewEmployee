9228130dbc050c8fa3a97dea6edba7c5cd5ebeed
masaya0731 <masaya0806543@gmail.com>
2022-01-24 08:50:15 +0000

    [Fix]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -14,34 +14,3 @@ class Customer < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :introduction, length: { maximum: 100 }
 end
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe Notification, type: :model do
-  describe 'アソシエーションのテスト' do
-    context 'Postモデル' do
-      it 'N:1になっているか' do
-        expect(Notification.reflect_on_association(:post).macro).to eq :belongs_to
-      end
-    end
-
-    context 'PostCommentモデル' do
-      it 'N:1になっているか' do
-        expect(Notification.reflect_on_association(:post_comment).macro).to eq :belongs_to
-      end
-    end
-
-    context '通知を送ったユーザー' do
-      it 'N:1になっているか' do
-        expect(Notification.reflect_on_association(:visitor).macro).to eq :belongs_to
-      end
-    end
-
-    context '通知が送られたユーザー' do
-      it 'N:1になっているか' do
-        expect(Notification.reflect_on_association(:visited).macro).to eq :belongs_to
-      end
-    end
-  end
-end
--- spec/factories/tag.rb
+++ spec/factories/tag.rb
@@ -1,20 +1,5 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe 'Tagモデルのテスト', type: :model do
-
-  describe 'アソシエーションのテスト' do
-    context 'TagMapモデル' do
-      it '1:Nとなっている' do
-        expect(Tag.reflect_on_association(:tag_maps).macro).to eq :has_many
-      end
-    end
-
-    context 'Postモデル' do
-      it '1:Nとなっている' do
-        expect(Tag.reflect_on_association(:posts).macro).to eq :has_many
-      end
-    end
+FactoryBot.define do
+  factory :tag do
+    name { "test" }
   end
 end
