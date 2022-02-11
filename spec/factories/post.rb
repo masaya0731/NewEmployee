7d626f58ad61fce6da599f6e7e26e08783b24e10
masaya0731 <masaya0806543@gmail.com>
2022-02-11 15:32:26 +0000

    [Update] テスト

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :post do
-    title { 'test' }
-    body { 'test' }
+    title { Faker::Lorem.characters(number:100) }
+    body { Faker::Lorem.characters(number:1000) }
   end
 end
