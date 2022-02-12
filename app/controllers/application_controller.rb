59b9c3a091d23e84078c299065c91449bc8c7b8b
masaya0731 <masaya0806543@gmail.com>
2022-02-12 15:52:52 +0000

    [Add] サクセスメッセージ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # 新規登録後
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "新規ユーザー登録しました"
+   root_path
+  end
+
+  # ログイン後
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "ログインしました"
+    root_path
+  end
+
+  # ログアウト後
+  def after_sign_out_path_for(resource)
+    flash[:notice] = "ログアウトしました"
+    root_path
+  end
+
   protected
 
   def configure_permitted_parameters
